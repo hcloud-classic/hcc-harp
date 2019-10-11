1416de9d03177ddf52ef08a6ae068b046ffedb5e
ish <ish@innogrid.com>
2019-10-11 15:31:38 +0900

    config: Fix comments of flute

--- config/flute.go
+++ config/flute.go
@@ -1,9 +1,9 @@
 package config
 
 type flute struct {
-	ServerAddress    string `goconf:"flute:flute_server_address"`     // ID : MySQL server login id
-	ServerPort       int64  `goconf:"flute:flute_server_port"`        // Password : MySQL server login password
-	RequestTimeoutMs int64  `goconf:"flute:flute_request_timeout_ms"` // Address : MySQL server address
+	ServerAddress    string `goconf:"flute:flute_server_address"`     // ServerAddress : IP address of server which installed flute module
+	ServerPort       int64  `goconf:"flute:flute_server_port"`        // ServerPort : Listening port number of flute module
+	RequestTimeoutMs int64  `goconf:"flute:flute_request_timeout_ms"` // RequestTimeoutMs : HTTP timeout for GraphQL request to flute module
 }
 
 // Flute : flute config structure
