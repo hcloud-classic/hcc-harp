cb23b233b1ded855d5c16884f52d3dd7084cee2b
ish <ish@innogrid.com>
2019-10-12 12:40:36 +0900

    types: Update subnet type

--- types/subnet.go
+++ types/subnet.go
@@ -4,12 +4,18 @@ import "time"
 
 // Subnet : Struct of Subnet
 type Subnet struct {
-	UUID      string    `json:"uuid"`
-	Name      string    `json:"name"`
-	NetworkIP string    `json:"network_ip"`
-	Netmask   string    `json:"netmask"`
-	Os        string    `json:"os"`
-	CreatedAt time.Time `json:"created_at"`
+	UUID           string    `json:"uuid"`
+	NetworkIP      string    `json:"network_ip"`
+	Netmask        string    `json:"netmask"`
+	Gateway        string    `json:"gateway"`
+	NextServer     string    `json:"next_server"`
+	NameServer     string    `json:"name_server"`
+	DomainName     string    `json:"domain_name"`
+	MaxNodes       string    `json:"max_nodes"`
+	LeaderNodeUUID string    `json:"leader_node_uuid"`
+	Os             string    `json:"os"`
+	Name           string    `json:"name"`
+	CreatedAt      time.Time `json:"created_at"`
 }
 
 // Subnets : Array struct of subnets
