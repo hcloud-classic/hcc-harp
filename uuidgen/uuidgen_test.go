15051fec054af045678b0b51e93ab2ad70059646
ish <ish@innogrid.com>
2019-10-14 16:50:19 +0900

    uuidgen: test: Fix test operation

--- uuidgen/uuidgen_test.go
+++ uuidgen/uuidgen_test.go
@@ -1,9 +1,36 @@
 package uuidgen
 
-import "testing"
+import (
+	"hcc/harp/checkroot"
+	"hcc/harp/config"
+	"hcc/harp/logger"
+	"hcc/harp/mysql"
+	"testing"
+)
 
-func Test_Uuidgen(t *testing.T) {
-	_, err := UUIDgen()
+func Test_UUIDgen(t *testing.T) {
+	if !checkroot.CheckRoot() {
+		t.Fatal("Failed to get root permission!")
+	}
+
+	if !logger.Prepare() {
+		t.Fatal("Failed to prepare logger!")
+	}
+	defer func() {
+		_ = logger.FpLog.Close()
+	}()
+
+	config.Parser()
+
+	err := mysql.Prepare()
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = mysql.Db.Close()
+	}()
+
+	_, err = UUIDgen()
 	if err != nil {
 		t.Fatal("Failed to generate uuid!")
 	}
