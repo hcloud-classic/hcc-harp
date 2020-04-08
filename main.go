e224cad6a755d0e1f1dd184050975867bf91e10b
ish <ish@innogrid.com>
2020-04-08 12:48:16 +0900

    [Hcloud-MiddleWare] harp build main struct

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func main() {
+
+	http.Handle("/graphql", graphql.GraphqlHandler)
+
+	logger.Logger.Println("Server is running on port " + strconv.Itoa(int(config.HTTP.Port)))
+	err = http.ListenAndServe(":"+strconv.Itoa(int(config.HTTP.Port)), nil)
+}
