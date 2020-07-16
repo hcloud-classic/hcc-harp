01c3959405997e63e042039cf78c5cf5040800c0
ish <ish@innogrid.com>
2020-07-16 11:55:51 +0900

    logger: Remove a test case that moved to fileutil

--- lib/logger/harpLogger_test.go
+++ lib/logger/harpLogger_test.go
@@ -5,13 +5,6 @@ import (
 	"testing"
 )
 
-func Test_CreateDirIfNotExist(t *testing.T) {
-	err := CreateDirIfNotExist("/var/log/" + LogName)
-	if err != nil {
-		t.Fatal("Failed to create dir!")
-	}
-}
-
 func Test_Logger_Prepare(t *testing.T) {
 	err := syscheck.CheckRoot()
 	if err != nil {
