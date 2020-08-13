cbbdec9c38425a13b2a504006d8a4fd1153da339
ish <ish@innogrid.com>
2020-08-13 15:15:13 +0900

    grpccli: Fix retry count logic

--- driver/grpccli/violinClient.go
+++ driver/grpccli/violinClient.go
@@ -18,12 +18,12 @@ func initViolin() error {
 
 	addr := config.Violin.ServerAddress + ":" + strconv.FormatInt(config.Violin.ServerPort, 10)
 	logger.Logger.Println("Trying to connect to violin module (" + addr + ")")
-	ctx, _ := context.WithTimeout(context.Background(), time.Duration(config.Violin.ConnectionTimeOutMs)*time.Millisecond)
 
 	for i := 0; i < int(config.Violin.ConnectionRetryCount); i++ {
+		ctx, _ := context.WithTimeout(context.Background(), time.Duration(config.Violin.ConnectionTimeOutMs)*time.Millisecond)
 		violinConn, err = grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
-			logger.Logger.Println("Failed to connect violin module ("+addr+"): %v", err)
+			logger.Logger.Println("Failed to connect violin module ("+addr+"): "+ err.Error())
 			logger.Logger.Println("Re-trying to connect to violin module (" +
 				strconv.Itoa(i+1) + "/" + strconv.Itoa(int(config.Violin.ConnectionRetryCount)) + ")")
 			continue
