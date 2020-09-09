b141968c49608f333943480652ae07a0dfe89759
ish <ish@innogrid.com>
2020-09-09 17:26:20 +0900

    [Hcloud-MiddleWare] Harp network vaild checker
    ipParse
    - Add a function to getting first and last IP addresses from network address.

--- lib/iputil/ipParse.go
+++ lib/iputil/ipParse.go
@@ -2,6 +2,7 @@ package iputil
 
 import (
 	"errors"
+	"github.com/apparentlymart/go-cidr/cidr"
 	"net"
 )
 
@@ -19,5 +20,25 @@ func CheckNetwork(networkIP string, networkNetmask string) (net.IP, net.IPMask, 
 	}
 
 	return netIPnetworkIP, mask, nil
+}
+
+// GetFirstAndLastIPs : Return first and last IP addresses from given network IP address and netmask.
+// Return as net.IP for both first and last IP addresses.
+func GetFirstAndLastIPs(networkIP string, networkNetmask string) (net.IP, net.IP, error) {
+	netIPnetworkIP, mask, err := CheckNetwork(networkIP, networkNetmask)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ipNet := net.IPNet{
+		IP:   netIPnetworkIP,
+		Mask: mask,
+	}
+
+	firstIP, lastIP := cidr.AddressRange(&ipNet)
+	firstIP = cidr.Inc(firstIP)
+	lastIP = cidr.Dec(lastIP)
+
+	return firstIP, lastIP, nil
 }
 
