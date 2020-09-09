d99e73e0ef7172c50c925741248397bd68462e90
ish <ish@innogrid.com>
2020-09-09 11:37:35 +0900

    grpc: errconv: Fix nil pointer error

--- action/grpc/errconv/errconv.go
+++ action/grpc/errconv/errconv.go
@@ -23,12 +23,11 @@ func GrpcStackToHcc(esg *[]*errg.HccError) *errh.HccErrorStack {
 	return errStack
 }
 
-func HccStackToGrpc(esh *errh.HccErrorStack) *[]*errg.HccError {
+func HccStackToGrpc(esh *errh.HccErrorStack) []*errg.HccError {
 	ges := []*errg.HccError{}
-	for i := esh.Len(); i > 0; i-- {
-		e := esh.Pop()
-		ge := &errg.HccError{ErrCode: e.ErrCode, ErrText: e.ErrText}
-		ges = append([]*errg.HccError{ge}, ges...)
+	for i := esh.Len(); i >= 0; i-- {
+		ge := &errg.HccError{ErrCode: (*esh)[i].ErrCode, ErrText: (*esh)[i].ErrText}
+		ges = append(ges, ge)
 	}
-	return &ges
+	return ges
 }
