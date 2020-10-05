48011496e8548a580d381025775080a5deb27333
ish <ish@innogrid.com>
2020-10-05 11:31:40 +0900

    mysql: Fix the test file

--- lib/mysql/connect_test.go
+++ lib/mysql/connect_test.go
@@ -1,14 +1,22 @@
 package mysql
 
 import (
+	"fmt"
 	"hcc/harp/lib/config"
 	"hcc/harp/lib/errors"
 	"hcc/harp/lib/logger"
+	"hcc/harp/lib/syscheck"
 	"testing"
 )
 
 func Test_DB_Prepare(t *testing.T) {
-	err := logger.Init()
+	err := syscheck.CheckOS()
+	if err != nil {
+		fmt.Println("Please run harp module on Linux or FreeBSD machine.")
+		panic(err)
+	}
+
+	err = logger.Init()
 	if err != nil {
 		errors.SetErrLogger(logger.Logger)
 		errors.NewHccError(errors.HarpInternalInitFail, "logger.Init(): "+err.Error()).Fatal()
