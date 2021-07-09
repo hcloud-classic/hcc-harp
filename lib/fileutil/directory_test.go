f95a719561f50555d0e1e640ee6beebc0f77f8ed
ish <ish@innogrid.com>
2021-07-09 17:25:18 +0900

    fileutil: Make 100% coverage of test files

--- lib/fileutil/directory_test.go
+++ lib/fileutil/directory_test.go
@@ -9,11 +9,21 @@ func Test_CreateDirIfNotExist(t *testing.T) {
 	if err != nil {
 		t.Fatal("Failed to create dir!")
 	}
+
+	err = CreateDirIfNotExist("/proc/harp_test")
+	if err != nil {
+		t.Log("Tried to create dir in /proc folder")
+	}
 }
 
 func Test_DeleteDir(t *testing.T) {
-	err := DeleteDir("/tmp/harp_test_dir")
+	err := DeleteDir("/tmp/harp_test")
 	if err != nil {
 		t.Fatal("Failed to delete dir!")
+	}
+
+	err = DeleteDir("/proc/cpuinfo")
+	if err != nil {
+		t.Log("Tried to delete file in /proc folder")
 	}
 }
