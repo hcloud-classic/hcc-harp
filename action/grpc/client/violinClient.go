c6e47c3b67f0b0b9a26c02c02b499301544b0e86
jollaman999 <admin@jollaman999.com>
2022-01-06 17:55:49 +0900

    grpc: Fix a wrong config reference in AllServerUUID()

--- action/grpc/client/violinClient.go
+++ action/grpc/client/violinClient.go
@@ -53,7 +53,7 @@ func (rc *RPCClient) AllServerUUID() ([]string, *hcc_errors.HccErrorStack) {
 	var errStack *hcc_errors.HccErrorStack
 
 	ctx, cancel := context.WithTimeout(context.Background(),
-		time.Duration(config.Flute.RequestTimeoutMs)*time.Millisecond)
+		time.Duration(config.Violin.RequestTimeoutMs)*time.Millisecond)
 	defer cancel()
 	resServerList, err := rc.violin.GetServerList(ctx, &pb.ReqGetServerList{})
 	if err != nil {
