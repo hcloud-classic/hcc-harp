541aa2fa285c70a50302c70a9de28915963e891b
jollaman999 <admin@jollaman999.com>
2022-01-10 15:42:08 +0900

    rsautil: Remove not used functions

--- lib/rsautil/rsaUtil.go
+++ lib/rsautil/rsaUtil.go
@@ -6,44 +6,7 @@ import (
 	"crypto/sha512"
 	"crypto/x509"
 	"encoding/pem"
-	"errors"
 )
-
-// GenerateKeyPair generates a new key pair
-func GenerateKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
-	privkey, err := rsa.GenerateKey(rand.Reader, bits)
-	if err != nil {
-		return nil, nil, err
-	}
-	return privkey, &privkey.PublicKey, nil
-}
-
-// PrivateKeyToBytes private key to bytes
-func PrivateKeyToBytes(priv *rsa.PrivateKey) []byte {
-	privBytes := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: x509.MarshalPKCS1PrivateKey(priv),
-		},
-	)
-
-	return privBytes
-}
-
-// PublicKeyToBytes public key to bytes
-func PublicKeyToBytes(pub *rsa.PublicKey) ([]byte, error) {
-	pubASN1, err := x509.MarshalPKIXPublicKey(pub)
-	if err != nil {
-		return nil, err
-	}
-
-	pubBytes := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PUBLIC KEY",
-		Bytes: pubASN1,
-	})
-
-	return pubBytes, nil
-}
 
 // BytesToPrivateKey bytes to private key
 func BytesToPrivateKey(priv []byte) (*rsa.PrivateKey, error) {
@@ -56,34 +19,6 @@ func BytesToPrivateKey(priv []byte) (*rsa.PrivateKey, error) {
 	}
 
 	return key, nil
-}
-
-// BytesToPublicKey bytes to public key
-func BytesToPublicKey(pub []byte) (*rsa.PublicKey, error) {
-	block, _ := pem.Decode(pub)
-	b := block.Bytes
-	var err error
-	ifc, err := x509.ParsePKIXPublicKey(b)
-	if err != nil {
-		return nil, err
-	}
-	key, ok := ifc.(*rsa.PublicKey)
-	if !ok {
-		return nil, errors.New("not ok")
-	}
-
-	return key, nil
-}
-
-// EncryptWithPublicKey encrypts data with public key
-func EncryptWithPublicKey(msg []byte, pub *rsa.PublicKey) ([]byte, error) {
-	hash := sha512.New()
-	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, pub, msg, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return ciphertext, nil
 }
 
 // DecryptWithPrivateKey decrypts data with private key
